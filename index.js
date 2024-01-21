91e58ff8059bf91790dd2a79cd67c00de1aaf43a
SantoshKumar <sksantosh1911@gmail.com>
2024-01-21 10:54:50 +0530

    first message added files

---
+++ index.js
@@ -0,0 +1,6 @@
+const age = 24;
+const name  = "sambangi"
+
+function sayName(){
+    console.log("may name is " + name)
+}
---
+++ server.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const server = () => {
+  return (
+    <div>
+      <h1>Santosh</h1>
+      <h2>Sambangi</h2>
+    </div>
+  );
+};
+
+export default server;
